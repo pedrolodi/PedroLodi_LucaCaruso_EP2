867414d52a027d394a94e1b00a1f7d1568837d55
Luca Caruso <lucac@al.insper.edu.br>
2021-11-03 18:18:35 -0300

    Inicio de jogo e imports

---
+++ Jogo_Domino.py
@@ -0,0 +1,10 @@
+#IMPORTANDO FUNÇÕES E BIBLIOTECAS
+from random import *
+from Functions import *
+
+pecas = []
+
+#COMEÇANDO JOGO
+n_jogadores = int(input('Quantos jogadores? '))
+
+inicia_jogo(n_jogadores,pecas)
