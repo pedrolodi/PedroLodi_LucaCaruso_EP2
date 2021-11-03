bd2b1d0ae9a5bfe19fbc2e38ae43929cb6c1be95
Pedro Lodi <pedrorl1@al.insper.edu.br>
2021-11-03 18:37:11 -0300

    Criando peças e validando n de jogadores

--- Jogo_Domino.py
+++ Jogo_Domino.py
@@ -2,9 +2,19 @@
 from random import *
 from Functions import *
 
-pecas = []
+# Criando pecas
+pecas = cria_pecas()
 
 #COMEÇANDO JOGO
 n_jogadores = int(input('Quantos jogadores? '))
+jogo_invalido = True
+while jogo_invalido == True:
+    if n_jogadores < 2 or n_jogadores > 4:
+        print('Número inválido, tente novamente')
+        n_jogadores = int(input('Quantos jogadores? '))
+    else:
+        jogo_invalido = False
 
-inicia_jogo(n_jogadores,pecas)
+inicio_jogo = inicia_jogo(n_jogadores, pecas)
+
+
