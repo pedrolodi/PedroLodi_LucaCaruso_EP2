232c6c57c830f31567a982f18e76c84e03376396
Luca Caruso <lucac@al.insper.edu.br>
2021-11-03 18:06:07 -0300

    Importando as 3 funções restantes

--- Functions.py
+++ Functions.py
@@ -1,5 +1,7 @@
-# Criando pecas:
+#Imports
 import random
+
+# Criando pecas:
 def cria_pecas():
     pecas = []
     for i in range(0, 7):
@@ -28,5 +30,112 @@ def posicoes_possiveis(mesa, pecas):
                 posicoes.append(pecas.index(peca))
             elif peca[0] == mesa[len(mesa)-1][1] or peca[1] == mesa[len(mesa)-1][1]:
                 posicoes.append(pecas.index(peca))
-    return posicoes 
-            
+    return posicoes
+
+#Iniciando Jogo de Dominó
+def inicia_jogo(n_jogadores,pecas):
+    #Criação de um dicionário com as listas de peças
+    jog_mes_mon = {
+        'jogadores':{},
+        'monte':[], 
+        'mesa':[]
+    }
+
+    #Criação de listas com as peças dos jogadores
+    P1 = []
+    P2 = []
+    P3 = []
+    P4 = []
+
+    #Peças embaralhadas
+    random.shuffle(pecas)
+
+    i=0
+    #Partida com 2 jogadores
+    if n_jogadores == 2:
+        while i<7:
+            add_P1 = P1.append(pecas[0])
+            del(pecas[0])
+            add_P2= P2.append(pecas[0])
+            del(pecas[0])
+            i = i+1
+        jog_mes_mon['jogadores'][1] = P1
+        jog_mes_mon['jogadores'][2] = P2
+
+    #Partida com 3 jogadores
+    if n_jogadores == 3:
+        while i<7:
+            add_P1 = P1.append(pecas[0])
+            del(pecas[0])
+            add_P2= P2.append(pecas[0])
+            del(pecas[0])
+            add_P3= P3.append(pecas[i])
+            del(pecas[0])
+            i = i+1
+        jog_mes_mon['jogadores'][1] = P1
+        jog_mes_mon['jogadores'][2] = P2
+        jog_mes_mon['jogadores'][3] = P3
+
+    #Partida com 4 jogadores
+    if n_jogadores == 4:
+        while i<7:
+            add_P1 = P1.append(pecas[0])
+            del(pecas[0])
+            add_P2= P2.append(pecas[0])
+            del(pecas[0])
+            add_P3= P3.append(pecas[0])
+            del(pecas[0])
+            add_P4= P4.append(pecas[0])
+            del(pecas[0])
+            i = i+1
+        jog_mes_mon['jogadores'][1] = P1
+        jog_mes_mon['jogadores'][2] = P2
+        jog_mes_mon['jogadores'][3] = P3
+        jog_mes_mon['jogadores'][4] = P4
+        
+    #Colocando as peças excedentes no monte
+    jog_mes_mon['monte'] = pecas
+
+    #Printando com formatação
+    for i in jog_mes_mon['jogadores']:
+        pecas_jogador = jog_mes_mon['jogadores'][i]
+        print('JOGADOR {}: {}'.format(i,pecas_jogador))
+    print("")
+    print('MONTE: {}'.format(jog_mes_mon['monte']))
+    print("")
+    print('MESA: {}'.format(jog_mes_mon['mesa']))
+
+    return jog_mes_mon
+
+#Adicionando Peças
+def adiciona_na_mesa(peca,mesa):
+    aux = []
+    final = mesa [-1][-1]
+    comeco = mesa [0][0]
+    if mesa == []:
+        mesa.append(peca)
+    elif comeco in peca:
+        if comeco == peca[0]:
+            aux.append(peca[::-1])
+            mesa= aux + mesa
+        elif comeco == peca[-1]:
+            aux.append(peca)
+            mesa = aux + mesa
+    elif final in peca:
+        if peca[0] == final:
+            mesa.append(peca)
+        elif peca[-1] == final: 
+            mesa.append(peca[::-1])
+    elif peca[0] == peca[1] and comeco in peca:
+        mesa.append(peca)
+
+    aux = []
+    return mesa
+
+#Soma peças
+def soma_pecas(lista):
+    soma = 0
+    for peca in lista:
+        soma = soma + peca[0]
+        soma = soma + peca[1]
+    return soma
